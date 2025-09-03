853c721e08f9a9511330ad2bf0d33de1d33b4191
Gaurav Vaidya <gaurav@renci.org>
2025-09-03 17:15:44 -0400

    Updated large DB scripts to download gzipped databases.

--- helm/redis-r3-external/scripts/backup-nn-redis-db3.sh
+++ helm/redis-r3-external/scripts/backup-nn-redis-db3.sh
@@ -21,9 +21,7 @@ function check_and_download() {
     echo Checking RDB file.
     kubectl exec -n "$NN_NAMESPACE" "nn-redis-$NN_VERSION-$name-master-0" -- bash -c 'redis-check-rdb /data/dump.rdb'
     # Backup file.
-    kubectl cp -n "$NN_NAMESPACE" "nn-redis-$NN_VERSION-$name-master-0:/data/dump.rdb" ./$name.rdb --retries 5 && \
-        md5 ./$name.rdb && \
-        gzip ./$name.rdb
+    kubectl cp -n "$NN_NAMESPACE" "nn-redis-$NN_VERSION-$name-master-0:/data/compressed.rdb.gz" ./$name.rdb.gz --retries 10
 }
 
 # Download files
